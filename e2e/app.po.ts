809432fc89a4d30fce63bb1d070814c204d89277
Roy Kass <roykwork@gmail.com>
2018-01-09 11:58:48 +0200

    added protractor e2e simple tests

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -8,4 +8,39 @@ export class AppPage {
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  getRightChevron() {
+    return element(by.css('.fa-chevron-right'));
+  }
+
+  getLeftChevron() {
+    return element(by.css('.fa-chevron-left'));
+  }
+
+  getFirstInput() {
+    return element.all(by.css('input')).first();
+  }
+
+  getFirstLabel() {
+    return element(by.css('label'));
+  }
+
+  getFirstTd(){
+    return element.all(by.css('td')).first();
+  }
+
+  getFirstCheckBox(){
+    return element.all(by.css('input[type="checkbox"]')).first();
+  }
+  getFirstButton(){
+    return element.all(by.css('button')).first();
+  }
+
+  getFirstTBody(){
+    return element.all(by.css('tbody')).first();
+  }
+
+  getTrCount() {
+    return element.all(by.css('tr')).count();
+  }
 }
